29a35f6a4d6504f92f7613fc50530fdb5eab4361
Carlos Pano H <carlospano8@gmail.com>
2021-11-14 19:42:59 -0600

    Commit Index

--- app-anuario/src/App.js
+++ app-anuario/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ import Header from './components/header';
+ import Feed from './components/feed';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Declaración de componente: Main Header - Logo, barra de búsqueda y Avatar */}
+      <Header />
+      {/* Feed General: Historias, menu, mensajes*/}
+      <Feed />
+
     </div>
   );
 }
