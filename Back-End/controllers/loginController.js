fffcc04c57d7ff93c9779ca46509b7b9e91ec9ba
FernandoFigueroa14 <luisfersurfer14@hotmail.com>
2021-11-25 14:45:47 -0600

    loginController

--- Back-End/controllers/loginController.js
+++ Back-End/controllers/loginController.js
@@ -1,46 +1,54 @@
 const bcryptjs = require('bcryptjs');
 const db = require('../database/models');
+const { validationResult } = require('express-validator');
 
 const Usuarios = db.Usuario;
 
 const loginController = {
     login: async (req, res) => {
-        await Usuarios.findOne({
-            where:{
-                email: req.body.email
-            }
-        })
-        .then(usuario => {
-            if(usuario){
-                let validationPassword = bcryptjs.compareSync(req.body.contraseña, usuario.contraseña);
-                if(validationPassword){
-                    req.session.userLogged = usuario;
-                    if(req.body.remember_user){
-                        res.cookie('email', req.body.email, {maxAge: (1000*60)*60});
-                    }
-                    return res.json({status: "200", msg: "Inicio de sesión exitoso", usuario});
-                }else{
-                    return res.json({
-                        status: 400,
-                        errors: {
-                            contraseña: {
-                                msg: 'Las credenciales son inválidas'
-                            }
-                        }
-                    })
-                }
-            }
-                
-            return res.json({
-                status: 400,
-                errors: {
-                    email: {
-                        msg: 'No se encuentra este correo electronico en nuestra base de datos'
-                    }
+        let errors = validationResult(req);
+
+        if(errors.isEmpty()){
+            await Usuarios.findOne({
+                where:{
+                    email: req.body.email
                 }
             })
-        })
-        .catch(error => res.json(error));
+            .then(usuario => {
+                if(usuario){
+                    let validationPassword = bcryptjs.compareSync(req.body.contraseña, usuario.contraseña);
+                    if(validationPassword){
+                        req.session.userLogged = usuario;
+                        if(req.body.remember_user){
+                            res.cookie('email', req.body.email, {maxAge: (1000*60)*60});
+                        }
+                        return res.json({status: "200", msg: "Inicio de sesión exitoso", usuario});
+                    }else{
+                        return res.json({
+                            status: 400,
+                            errors: {
+                                contraseña: {
+                                    msg: 'Las credenciales son inválidas'
+                                }
+                            }
+                        })
+                    }
+                }
+                    
+                return res.json({
+                    status: 400,
+                    errors: {
+                        email: {
+                            msg: 'No se encuentra este correo electronico en nuestra base de datos'
+                        }
+                    }
+                })
+            })
+            .catch(error => res.json({status: 400, errors: error}));
+         }else {
+            // Si hay errores
+            res.json({status: 400, errors: errors.mapped()})
+        }
     }
 }
 
