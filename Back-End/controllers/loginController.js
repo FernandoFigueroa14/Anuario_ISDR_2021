759f715de7d0a39138900e2dbcb06ebe58802485
FernandoFigueroa14 <luisfersurfer14@hotmail.com>
2021-11-26 01:27:46 -0600

    cookie

--- Back-End/controllers/loginController.js
+++ Back-End/controllers/loginController.js
@@ -21,7 +21,7 @@ const loginController = {
                     if(validationPassword){
                         req.session.userLogged = usuario;
                         if(req.body.remember_user){
-                            res.cookie('email', req.body.email, {maxAge: (1000*60)*60, sameSite: true});
+                            res.cookie('email', req.body.email, {maxAge: (1000*60)*60, sameSite: 'None'});
                         }
                         return res.json({status: 200, msg: "Inicio de sesión exitoso", usuario});
                     }else{
