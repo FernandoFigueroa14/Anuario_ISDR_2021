34de9f9380ba6d415bd189171178f6ba5e5c8601
FernandoFigueroa14 <luisfersurfer14@hotmail.com>
2021-11-26 02:42:50 -0600

    session

--- Back-End/public/js/userLoggedMiddleware.js
+++ Back-End/public/js/userLoggedMiddleware.js
@@ -3,6 +3,8 @@ const db = require('../../database/models');
 const Usuarios = db.Usuario;
 
 function userLoggedMiddleware(req, res, next){
+    console.log(req.session.userLogged);
+    console.log(req.cookies);
     if(req.cookies.email){
         let cookieEmail = req.cookies.email;   
 
